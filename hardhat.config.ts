51bf21e9c1cdf9022326b2445ec827c738835890
Vyacheslav Ryzhkov <rugrisser@gmail.com>
2025-04-02 19:28:22 +0300

    feat: add infura deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -12,6 +15,12 @@ const config: HardhatUserConfig = {
       viaIR: true,
     }
   },
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  }
 };
 
 export default config;
